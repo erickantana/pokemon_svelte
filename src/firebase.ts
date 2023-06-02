225a2154ffdfcb02f045bc31973053a9d13baee5
Eric Kantana <rafael.eric.kantana@gmail.com>
2023-06-02 19:13:08 +0700

    Initial commit and adding google authentication using firebase auth

---
+++ src/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { GoogleAuthProvider, getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyATAoTnJci9qdE-BT1BMTOeHbF0Tzkey3w',
+	authDomain: 'pokemon-f3521.firebaseapp.com',
+	projectId: 'pokemon-f3521',
+	storageBucket: 'pokemon-f3521.appspot.com',
+	messagingSenderId: '137600164108',
+	appId: '1:137600164108:web:0e15241de151cadab2e929',
+	measurementId: 'G-4971EMD929'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+export const googleAuthProvider = new GoogleAuthProvider();
